ca70f43e6f9a111d48eb1027fbd500a81a90826c
Agustina-Cavallaro <mcavallaro@udesa.edu.ar>
2025-09-06 16:40:48 -0300

    Components and foot

---
+++ proyectointegrador3/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./stylesF.css"
+
+function Footer(props) {
+    return(
+        <footer>
+             <p>Maria Agustina Cavallaro y Ana Paula Oldani </p>
+        </footer>
+    )
+}
+
+export default Footer
